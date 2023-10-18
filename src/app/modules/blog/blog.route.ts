60f1e095d625f6b2b591bdfe7ccb8fe21aa2b83f
masudranadev <masudranadev51@gmail.com>
2023-10-18 06:52:43 +0600

    some code is updated

--- src/app/modules/blog/blog.route.ts
+++ src/app/modules/blog/blog.route.ts
@@ -8,11 +8,7 @@ import { BlogValidation } from './blog.validation';
 const router = Router();
 
 router.get('/', BlogController.getBlogs);
-router.get(
-  '/:id',
-  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.USER),
-  BlogController.getBlog
-);
+router.get('/:id', BlogController.getBlog);
 
 router.delete(
   '/:id',
